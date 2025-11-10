ffbfc7eba7948a81c7e9b2065da1a71e6668df41
Tumbleweed <tuumbleweed@users.noreply.github.com>
2025-11-10 08:46:11 -0500

    Add xerr package

---
+++ init.go
@@ -0,0 +1,47 @@
+// Everything that works with raw err error
+// allows us to wrap NewError calls in one line
+package xerr
+
+import tl "github.com/meeeraaakiii/tintlog/logger"
+
+/*
+If err is nil - do nothing.
+If it's not nil - use NewError and then PrintError.
+
+Without context.
+*/
+func QuitIfError(err error, msg string) {
+	if err == nil {
+		return
+	}
+	NewError(err, msg, "").Print("error", tl.Critical, 1)
+}
+
+/*
+If err is nil - do nothing.
+If it's not nil - use NewError and then PrintError.
+
+Without context.
+*/
+func QuitIfErrorWithContext(err error, msg, context string) {
+	if err == nil {
+		return
+	}
+	NewError(err, msg, context).PrintWithContext("error", tl.Critical, 1)
+}
+
+// same as QuitIfError but errorType is a parameter
+func QuitIf(errorType ErrorType, err error, msg string) {
+	if err == nil {
+		return
+	}
+	NewError(err, msg, "").Print(errorType, tl.Critical, 1)
+}
+
+// same as QuitIf but don't stop the program
+func Print(errorType ErrorType, err error, msg string) {
+	if err == nil {
+		return
+	}
+	NewError(err, msg, "").Print(errorType, tl.Critical, 0)
+}
